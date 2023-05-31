ce324f5b24139928bd5f5a57747bf2ec31259632
nombre_de_usuario_1 <nicolasyracing@gmail.com>
2023-05-31 16:59:00 -0300

    ejercicio harry potter

--- src/Library.hs
+++ src/Library.hs
@@ -4,105 +4,153 @@ import PdePreludat
 doble :: Number -> Number
 doble numero = numero + numero
 
-type Material = String
-data Personaje = UnPersonaje {
-    nombre:: String,
-    puntaje:: Number,
-    inventario:: [Material]
-} deriving Show
-type Tiempo = Number
-
-data Receta = UnaReceta {
-    materiales:: [Material],
-    tiempo :: Tiempo,
-    nombreReceta :: String
+type Promedio = Number
+type Sabor = String
+data Postre = UnPostre {
+    sabores :: [Sabor],
+    peso :: Number,
+    temperatura:: Number
 } deriving Show
 
-fogata :: Receta
-fogata = UnaReceta {
-    materiales= ["madera","fosforo"],
-    tiempo = 10,
-    nombreReceta = "fogata"
+type Hechizo = Postre -> Postre
+
+flan :: Postre
+flan = UnPostre {
+    sabores = ["ascas"],
+    peso= 500,
+    temperatura = 10
 }
 
-pollo :: Receta
-pollo = UnaReceta {
-    materiales= ["pollo","fogata"],
-    tiempo = 300,
-    nombreReceta = "pollo"
+incendio :: Hechizo
+incendio postre = postre {
+    temperatura = temperatura postre + 1,
+    peso = peso postre * 0.95
 }
 
-steve:: Personaje
-steve = UnPersonaje {
-    nombre= "hola",
-    puntaje= 1000,
-    inventario  = ["fogata"]
+immobulus :: Hechizo
+immobulus postre = postre {
+    temperatura = 0
 }
 
-craftearReceta:: Receta -> Personaje -> Personaje
-craftearReceta receta unPersonaje
-    | tieneLosMateriales receta unPersonaje = (actualizarPuntaje receta . sacarMateriales receta . agregarNuevoMaterial (nombreReceta receta) ) unPersonaje
-    | otherwise = unPersonaje { puntaje = puntaje unPersonaje - 100 }
-
-sacarMateriales:: Receta -> Personaje -> Personaje
-sacarMateriales receta unPersonaje = unPersonaje { inventario = quitarMaterialesDelInventario receta (inventario unPersonaje)}
-
---Por ahora, quita todas las unidades de los materiales, aunque solo deberia quitarlos una vez
-quitarMaterialesDelInventario:: Receta -> [Material] -> [Material]
-quitarMaterialesDelInventario receta = filter (\mat -> mat `notElem` materiales receta)
-
-tieneLosMateriales :: Receta -> Personaje -> Bool
-tieneLosMateriales receta unPersonaje = all (\a -> a `elem` inventario unPersonaje) (materiales receta)
-
-agregarNuevoMaterial:: Material -> Personaje -> Personaje
-agregarNuevoMaterial material unPersonaje = unPersonaje { inventario = inventario unPersonaje ++ [material]}
-
-actualizarPuntaje:: Receta-> Personaje-> Personaje
-actualizarPuntaje receta unPersonaje = unPersonaje { puntaje = puntaje unPersonaje + 10 * tiempo receta}
-
-duplicaPuntaje:: Receta -> Personaje -> Bool
-duplicaPuntaje receta unPersonaje = puntaje unPersonaje + 10 * tiempo receta >= 2 * puntaje unPersonaje
-
-obtenerCrafteables:: [Receta] -> Personaje -> [String]
-obtenerCrafteables recetas unPersonaje = map nombreReceta (filter (\unaReceta -> tieneLosMateriales unaReceta unPersonaje && duplicaPuntaje unaReceta unPersonaje) recetas)
-
-craftearListaRecetas :: [Receta] -> Personaje ->Personaje
-craftearListaRecetas recetas unPersonaje = foldl (\pers receta -> craftearReceta receta pers) unPersonaje recetas
-
--- 2c no lo hice
-
--- Mine 
-data Bioma = UnBioma {
-    materialNecesario :: Material,
-    materialesBioma :: [Material]
-} deriving Show
-
-artico :: Bioma
-artico = UnBioma {
-    materialNecesario = "sueter",
-    materialesBioma = ["hielo","iglu", "lobo"]
+wingardium :: Hechizo
+wingardium postre = postre {
+    sabores = sabores postre ++ ["concentrado"],
+    peso = peso postre * 0.90
 }
 
-type Herramienta = [Material] -> Material
+diffindo:: Number -> Hechizo
+diffindo porcentaje postre = postre {
+    peso = peso postre * ((100 - porcentaje)/100)
+}
 
-hacha :: Herramienta
-hacha = last
+riddikulus:: Sabor -> Hechizo
+riddikulus sabor postre = postre {
+    sabores = sabores postre ++ [reverse sabor]
+}
 
-espada :: Herramienta
-espada = head 
+vaciarSabores:: Hechizo
+vaciarSabores postre = postre { sabores = []}
 
-pico :: Number -> Herramienta
-pico pos mats = mats !! pos  
+avada:: Hechizo
+avada = immobulus . vaciarSabores
 
 
-tieneMaterialNecesario:: Material -> Personaje ->Bool
-tieneMaterialNecesario mat unPersonaje = elem mat (inventario unPersonaje)
+-- Punto C
 
-sumarPuntos:: Number -> Personaje -> Personaje
-sumarPuntos puntos unPersonaje = unPersonaje { puntaje = puntaje unPersonaje + puntos}
-minar:: Herramienta -> Personaje -> Bioma -> Personaje
-minar unaHerramienta unPersonaje unBioma
-    | tieneMaterialNecesario (materialNecesario unBioma) unPersonaje = (agregarNuevoMaterial (unaHerramienta (materialesBioma unBioma)) . sumarPuntos 50) unPersonaje
-    | otherwise = unPersonaje
+pesaMasQueCero:: Postre -> Bool
+pesaMasQueCero postre = peso postre > 0
 
--- me falta el 2 y el 3
+tieneAlgunSabor:: Postre -> Bool
+tieneAlgunSabor postre = length (sabores postre) > 0
+
+noEstaCongelado:: Postre -> Bool
+noEstaCongelado postre = temperatura postre > 0
+
+estaPostreListo:: Postre -> Bool
+estaPostreListo postre = pesaMasQueCero postre &&  tieneAlgunSabor postre &&  noEstaCongelado postre
+
+saberSiQuedanListos:: Hechizo -> [Postre] -> Bool
+saberSiQuedanListos hechizo = all (estaPostreListo . hechizo)
+
+-- Punto D
+-- Dado un conjunto de postres en la mesa, conocer el peso promedio de los postres listos. 
+promedioPesoPostresListos:: [Postre] -> Promedio
+promedioPesoPostresListos postres = obtenerPromedioPeso (filter estaPostreListo postres)
+
+obtenerPesoPostre:: Postre -> Number
+obtenerPesoPostre  = peso
+
+obtenerPromedioPeso:: [Postre] -> Promedio
+obtenerPromedioPeso postres = sum (map obtenerPesoPostre postres) / length postres
+
+-- Punto 2A
+-- Hacer que un mago asista a la clase de defensa contra las cocinas oscuras y practique con un hechizo sobre un postre (se espera obtener el mago).
+-- Cuando un mago practica con un hechizo, lo agrega a sus hechizos aprendidos. 
+-- Además si el resultado de usar el hechizo en el postre es el mismo que aplicarle “avada kedavra” al postre, entonces suma un horrorcrux.
+
+data Mago = UnMago {
+    hechizosAprendidos:: [Hechizo],
+    cantidadDeHorrocruxes:: Number
+}deriving Show
+
+hp = UnMago {
+    hechizosAprendidos = [],
+    cantidadDeHorrocruxes = 0
+}
+
+
+aprenderHechizo:: Hechizo -> Mago -> Mago
+aprenderHechizo hechizo mago = mago { hechizosAprendidos = hechizosAprendidos mago ++ [hechizo]}
+
+sonPostresIguales:: Postre -> Postre -> Bool
+sonPostresIguales postre1 postre2 = peso postre1 == peso postre2 &&  temperatura postre1 == temperatura postre2 && sabores postre1 == sabores postre2
+
+sumarONoHorrocrux:: Hechizo -> Postre -> Mago -> Mago
+sumarONoHorrocrux hechizo postre mago
+    | sonPostresIguales (hechizo postre) (avada postre) = mago { cantidadDeHorrocruxes = cantidadDeHorrocruxes mago + 1}
+    | otherwise = mago
+
+practicarHechizo:: Hechizo -> Postre -> Mago -> Mago
+practicarHechizo hechizo postre  = aprenderHechizo hechizo . sumarONoHorrocrux hechizo postre
+
+-- Punto 2B
+-- Dado un postre y un mago obtener su mejor hechizo, que es aquel de sus hechizos que deja al postre con más cantidad de sabores luego de usarlo.
+
+--Lo saque de la resolucion porque no lo entendia
+
+mejorHechizoV1 :: Postre -> Mago -> Hechizo
+mejorHechizoV1 postre mago = elMejor postre (hechizosAprendidos mago)
+
+elMejor :: Postre -> [Hechizo] -> Hechizo
+elMejor postre [hechizo] = hechizo
+elMejor postre (primer:segundo:restohechizos) | esMejor postre primer segundo = elMejor postre (primer:restohechizos)
+    | otherwise = elMejor postre (segundo:restohechizos)
+
+esMejor :: Postre -> Hechizo -> Hechizo -> Bool
+esMejor postre hechizo1 hechizo2 = (length . sabores . hechizo1) postre > (length . sabores . hechizo2) postre
+
+
+-- Punto 3A
+
+obtenerListaDeAlgo:: a -> [a]
+obtenerListaDeAlgo a = a : obtenerListaDeAlgo a
+
+obtenerListaDePostres:: Postre -> [Postre]
+obtenerListaDePostres = obtenerListaDeAlgo
+
+obtenerMagoConInfinitosPoderes:: Mago -> Hechizo -> Mago
+obtenerMagoConInfinitosPoderes mago hechizo = mago { hechizosAprendidos = obtenerListaDeAlgo hechizo }
+
+-- Punto 3B
+-- Suponiendo que hay una mesa con infinitos postres, y pregunto si algún hechizo los deja listos
+-- ¿Existe alguna consulta que pueda hacer para que me sepa dar una respuesta? Justificar conceptualmente.
+
+-- Se podría ir preguntando uno a uno si los deja listos sin problema, ya que haskel va mostrando el resultado a medida que avanza la lista. Sería una
+-- lista infinita de True o False.
+
+
+-- Punto 3C 
+-- Suponiendo que un mago tiene infinitos hechizos
+-- ¿Existe algún caso en el que se puede encontrar al mejor hechizo? Justificar conceptualmente.
+
+-- Es imposible encontrar el mejor hechizo porque lo deberia comprar contra todos los demas, y al ser una lista infinita nunca podria terminar de comprararlos.
---
+++ src/minecraft.hs
@@ -0,0 +1,108 @@
+-- module Library where
+-- import PdePreludat
+
+-- doble :: Number -> Number
+-- doble numero = numero + numero
+
+-- type Material = String
+-- data Personaje = UnPersonaje {
+--     nombre:: String,
+--     puntaje:: Number,
+--     inventario:: [Material]
+-- } deriving Show
+-- type Tiempo = Number
+
+-- data Receta = UnaReceta {
+--     materiales:: [Material],
+--     tiempo :: Tiempo,
+--     nombreReceta :: String
+-- } deriving Show
+
+-- fogata :: Receta
+-- fogata = UnaReceta {
+--     materiales= ["madera","fosforo"],
+--     tiempo = 10,
+--     nombreReceta = "fogata"
+-- }
+
+-- pollo :: Receta
+-- pollo = UnaReceta {
+--     materiales= ["pollo","fogata"],
+--     tiempo = 300,
+--     nombreReceta = "pollo"
+-- }
+
+-- steve:: Personaje
+-- steve = UnPersonaje {
+--     nombre= "hola",
+--     puntaje= 1000,
+--     inventario  = ["fogata"]
+-- }
+
+-- craftearReceta:: Receta -> Personaje -> Personaje
+-- craftearReceta receta unPersonaje
+--     | tieneLosMateriales receta unPersonaje = (actualizarPuntaje receta . sacarMateriales receta . agregarNuevoMaterial (nombreReceta receta) ) unPersonaje
+--     | otherwise = unPersonaje { puntaje = puntaje unPersonaje - 100 }
+
+-- sacarMateriales:: Receta -> Personaje -> Personaje
+-- sacarMateriales receta unPersonaje = unPersonaje { inventario = quitarMaterialesDelInventario receta (inventario unPersonaje)}
+
+-- --Por ahora, quita todas las unidades de los materiales, aunque solo deberia quitarlos una vez
+-- quitarMaterialesDelInventario:: Receta -> [Material] -> [Material]
+-- quitarMaterialesDelInventario receta = filter (\mat -> mat `notElem` materiales receta)
+
+-- tieneLosMateriales :: Receta -> Personaje -> Bool
+-- tieneLosMateriales receta unPersonaje = all (\a -> a `elem` inventario unPersonaje) (materiales receta)
+
+-- agregarNuevoMaterial:: Material -> Personaje -> Personaje
+-- agregarNuevoMaterial material unPersonaje = unPersonaje { inventario = inventario unPersonaje ++ [material]}
+
+-- actualizarPuntaje:: Receta-> Personaje-> Personaje
+-- actualizarPuntaje receta unPersonaje = unPersonaje { puntaje = puntaje unPersonaje + 10 * tiempo receta}
+
+-- duplicaPuntaje:: Receta -> Personaje -> Bool
+-- duplicaPuntaje receta unPersonaje = puntaje unPersonaje + 10 * tiempo receta >= 2 * puntaje unPersonaje
+
+-- obtenerCrafteables:: [Receta] -> Personaje -> [String]
+-- obtenerCrafteables recetas unPersonaje = map nombreReceta (filter (\unaReceta -> tieneLosMateriales unaReceta unPersonaje && duplicaPuntaje unaReceta unPersonaje) recetas)
+
+-- craftearListaRecetas :: [Receta] -> Personaje ->Personaje
+-- craftearListaRecetas recetas unPersonaje = foldl (\pers receta -> craftearReceta receta pers) unPersonaje recetas
+
+-- -- 2c no lo hice
+
+-- -- Mine 
+-- data Bioma = UnBioma {
+--     materialNecesario :: Material,
+--     materialesBioma :: [Material]
+-- } deriving Show
+
+-- artico :: Bioma
+-- artico = UnBioma {
+--     materialNecesario = "sueter",
+--     materialesBioma = ["hielo","iglu", "lobo"]
+-- }
+
+-- type Herramienta = [Material] -> Material
+
+-- hacha :: Herramienta
+-- hacha = last
+
+-- espada :: Herramienta
+-- espada = head 
+
+-- pico :: Number -> Herramienta
+-- pico pos mats = mats !! pos  
+
+
+-- tieneMaterialNecesario:: Material -> Personaje ->Bool
+-- tieneMaterialNecesario mat unPersonaje = elem mat (inventario unPersonaje)
+
+-- sumarPuntos:: Number -> Personaje -> Personaje
+-- sumarPuntos puntos unPersonaje = unPersonaje { puntaje = puntaje unPersonaje + puntos}
+-- minar:: Herramienta -> Personaje -> Bioma -> Personaje
+-- minar unaHerramienta unPersonaje unBioma
+--     | tieneMaterialNecesario (materialNecesario unBioma) unPersonaje = (agregarNuevoMaterial (unaHerramienta (materialesBioma unBioma)) . sumarPuntos 50) unPersonaje
+--     | otherwise = unPersonaje
+
+-- -- me falta el 2 y el 3
